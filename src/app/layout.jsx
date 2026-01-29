a7db71df1e0dcf4776d59d9582875d76d6657ffb
Will Yip <will@cloudful.io>
2026-01-28 20:25:11 -0500

    Added favicons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -31,7 +31,11 @@ export default async function RootLayout({ children }) {
       <Head
       // ... Your additional head options
       >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+        <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/images/favicon/site.webmanifest" />
       </Head>
       <body>
         <Layout
