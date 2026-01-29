a4f1df241ca007c63f06a613e16b60c3884186c9
Will Yip <will@cloudful.io>
2026-01-28 19:55:58 -0500

    Updated doc repo base url

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -38,8 +38,9 @@ export default async function RootLayout({ children }) {
           //banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/cloudful-io/help.visuallyme.com/tree/main/src/content"
           footer={footer}
+          
           // ... Your additional layout options
         >
           {children}
